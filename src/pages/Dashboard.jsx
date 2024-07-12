755e128642dedb5f5b42eff7c8661f242d74ce6b
devJawad1 <adekanbija1@gmail.com>
2024-07-12 13:46:38 +0100

    addeing dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
           <div className="p-0 pt-2 col-md-5">
             <div className="card bg-primary col-2 col-md-1" style={{ height: "50px", borderRadius: "50%" }}></div>
             <div>
-              <h3 className='greeting text-white pt-3'>Saheed, <br />Welcome to Auto wash</h3>
+              <h3 className='greeting text-white pt-3'>Jawad, <br />Welcome to Auto wash</h3>
             </div>
           </div>
           <div className="text-white d-flex pt-2" style={{ gap: "10px" }}>
@@ -85,24 +85,93 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        <div className="shadow p-2 bg-white mt-4 mx-3 mx-md-4 rounded">
-          <div className="d-md-flex">
+
+        <div className="shadow p-md-2 pt-2 bg-white mt-4 mx-3 mx-md-4 rounded m-4">
+          <h5 className='px-2'>Just for you</h5>
+          <div className="d-md-flex mt-2">
             <div className="car col-md-6">
-              <div className="d-flex justify-content-between border-bottom">
-                <h5>Your car</h5>
-                <h5 class="bi bi-plus-circle"></h5>
-              </div>
-              <div className="car-list">
-                {/* <p>You have no car</p> */}
-                <div className="d-flex justify-content-between border-bottom mt-2 bg-ligh p-1 pt-2">
-                  <h6>Toyora camry</h6>
-                  <i class="bi bi-pencil-square"></i>
+              <div className="">
+                <div className="d-flex justify-content-between border-bottom">
+                  <h5>Your car</h5>
+                  <h5 class="bi bi-plus-circle"></h5>
                 </div>
-                <div className="d-flex justify-content-between border-bottom mt- bg-ligh p-1 pt-2">
-                  <h6>Lexus 350</h6>
-                  <i class="bi bi-pencil-square"></i>
+                <div className="car-list">
+                  {/* <p>You have no car</p> */}
+                  <div className="d-flex justify-content-between border-bottom mt-2 bg-ligh p-1 pt-2">
+                    <h6>Toyora camry</h6>
+                    <i class="bi bi-pencil-square"></i>
+                  </div>
+                  <div className="d-flex justify-content-between border-bottom mt- bg-ligh p-1 pt-2">
+                    <h6>Lexus 350</h6>
+                    <i class="bi bi-pencil-square"></i>
+                  </div>
+                  <div className="d-flex justify-content-between border-bottom mt- bg-ligh p-1 pt-2">
+                    <h6>Lexus 350</h6>
+                    <i class="bi bi-pencil-square"></i>
+                  </div>
+                  <div className="d-flex justify-content-between border-bottom mt- bg-ligh p-1 pt-2">
+                    <h6>Lexus 350</h6>
+                    <i class="bi bi-pencil-square"></i>
+                  </div>
                 </div>
               </div>
+            </div>
+            <div className="col-md-6 px-2">
+              <div className="px-2 p-2 p-md-0">
+                <div className="d-flex justify-content-between mt-4 mt-md-0">
+                  <h5>Quick links</h5>
+                  <h5 class="bi bi-link"></h5>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-car-front-fill"></h5>
+                  </div>
+                  <h6 className='pt-2'>View cars</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-person-add"></h5>
+                  </div>
+                  <h6 className='pt-2'>Choose management</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-person"></h5>
+                  </div>
+                  <h6 className='pt-2'>Membership status</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-journal-text"></h5>
+                  </div>
+                  <h6 className='pt-2'>View all history</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="fourth-section">
+          <div className="d-md-flex mx-4 m-4">
+            <div className="col-md-6 px-1">
+              <div className="shadow p-2 rounded">
+                <div className="form1">
+                  <form action="" className='p-4'>
+                  <h5>Book Appointment</h5>
+                    <div className="inputBox mt-3">
+                      <input id="form1-name" required type="text" className=''/>
+                      <label htmlFor="form1-name">Name</label>
+                    </div>
+                    <div className="" style={{float:"right"}}>
+                    <button type="submit" className='mt-2'>Book now</button>
+
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <h5>Recent history</h5>
             </div>
           </div>
         </div>
