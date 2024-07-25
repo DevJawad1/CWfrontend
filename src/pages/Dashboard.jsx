fd21ea07c8c36070200adf2e1fa89858c644e3df
devJawad1 <adekanbija1@gmail.com>
2024-07-25 09:54:01 +0100

    making changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -135,7 +135,7 @@ const Dashboard = () => {
                   <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
                     <h5 class="bi bi-car-front-fill"></h5>
                   </div>
-                  <h6 className='pt-2'>View cars</h6>
+                  <h6 className='pt-2'>My cars</h6>
                 </div>
                 <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }} onClick={()=>{navigate('/chooseManagement')}}>
                   <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
