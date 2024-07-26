02c73b7148c231450da6ccdf91e279fc4bbb0005
devJawad1 <adekanbija1@gmail.com>
2024-07-26 11:29:07 +0100

    adding sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom'
 const Sidebar = () => {
+    const navigate = useNavigate()
     return (
         <div>
             {/* <a class="btn btn-primary fs-4 p-0 bi bi-list" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" style={{height:"30px"}}> */}
@@ -10,25 +11,56 @@ const Sidebar = () => {
             <h4 className='text-white bg-primary d-flex justify-content-center col-12' data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
                 <i className='bi bi-list ' style={{paddingLeft:"-200px"}}></i>
             </h4>
+            {/* p-2 rounded bg-light     */}
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Explore </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <div class="offcanvas-body">
-                    <div>
-                        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-                    </div>
-                    <div class="dropdown mt-3">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                            Dropdown button
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </div>
+                <div class="offcanvas-body border-top p-0 px-2">
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }} onClick={()=>{navigate('/mycar')}}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-car-front-fill"></h5>
+                  </div>
+                  <h6 className='pt-2'>My cars</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }} onClick={()=>{navigate('/chooseManagement')}}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-person-add"></h5>
+                  </div>
+                  <h6 className='pt-2'>Choose management</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-person"></h5>
+                  </div>
+                  <h6 className='pt-2'>Membership status</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-journal-text"></h5>
+                  </div>
+                  <h6 className='pt-2'>My history</h6>
+                </div>
+
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }} onClick={()=>{navigate('/uploadcar')}}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-car-front-fill"></h5>
+                  </div>
+                  <h6 className='pt-2'>Upload car</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }} onClick={()=>{navigate('/chooseManagement')}}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-person-add"></h5>
+                  </div>
+                  <h6 className='pt-2'>Book Appointment</h6>
+                </div>
+                <div className="d-flex align-items-center mt-2" style={{ gap: "10px" }}>
+                  <div className="shadow-sm d-flex justify-content-center p-1 px-2" style={{ borderRadius: "50%", height: "35px" }}>
+                    <h5 class="bi bi-person"></h5>
+                  </div>
+                  <h6 className='pt-2'>Membership status</h6>
+                </div>
                 </div>
             </div>
         </div>
