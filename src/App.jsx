aea7290c4d9fb090d3b87d48a6f033a0d5838ff1
devJawad1 <adekanbija1@gmail.com>
2024-07-26 10:42:34 +0100

    on my car page

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
     <ToastContainer />
      <Routes>
         <Route path='/home' element={<LandingPage/>}/>
+        <Route path='/home' element={<LandingPage/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/loading' element={<Loading/>}/>
