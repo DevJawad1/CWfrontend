1b9897a5ec26e3405c9880d3a653e24664990b84
devJawad1 <adekanbija1@gmail.com>
2024-08-29 19:24:59 +0100

    working on book car page

--- src/pages/BookCarforWash.jsx
+++ src/pages/BookCarforWash.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import GreetingLabel from '../component/GreetingLabel'
 import Bluebackground from '../component/Bluebackground'
-
+import './bookcar.css'
 const BookCarforWash = () => {
     const [allCar, setallCar] = useState([])
     const navigate = useNavigate()
@@ -21,8 +21,25 @@ const BookCarforWash = () => {
     <div>
         <Bluebackground/> 
         <div className="position-absolute dashboard w-100" style={{ top: "0", zIndex: "2" }}>
-        <GreetingLabel msg={"Book car"}/>
-              
+            <GreetingLabel msg={"Book car"}/>
+
+            <div className="p-2 px-3">
+            <h5 className='text-white'>Select the car you are washing, you can select more than one car for wash   </h5>
+            <div className="car-display bookcar d-flex flex-wrap">
+                {
+                    allCar.map((car, i)=>(
+                        <div className="col-4 col-md-2 px-md-2 px-1 pt-0 mt-2">
+                            <div className='p-2 bg-white rounded'>
+                                <div className="car-img col-12 shadow bg-white rounded " style={{backgroundImage:`url(${car.image})`}}>
+
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+            </div>
         </div>
     </div>
   )
