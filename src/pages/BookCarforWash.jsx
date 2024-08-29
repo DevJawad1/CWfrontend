cfd0e61560e5f3c214d0bfdf02e4dfaf89e48d3f
devJawad1 <adekanbija1@gmail.com>
2024-08-29 18:27:19 +0100

    adding book car page

---
+++ src/pages/BookCarforWash.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import GreetingLabel from '../component/GreetingLabel'
+import Bluebackground from '../component/Bluebackground'
+
+const BookCarforWash = () => {
+    const [allCar, setallCar] = useState([])
+    const navigate = useNavigate()
+    const getAllcar = async () => {
+        const myCar = await axios.post("https://cw-backend-five.vercel.app/member/myCar", { user: localStorage.cwUser })
+        console.log(myCar.data);
+        if (myCar.data.status) {
+            setallCar(myCar.data.myCar)
+        }
+    }
+    useEffect(() => {
+        getAllcar()
+    }, [])
+  return (
+    <div>
+        <Bluebackground/> 
+        <GreetingLabel msg={"Book car"}/>
+        <div className="position-absolute dashboard w-100" style={{ top: "0", zIndex: "2" }}>
+                
+        </div>
+    </div>
+  )
+}
+
+export default BookCarforWash
