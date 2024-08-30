d8304f633e8612a0caf54b0a98168f52ef45e53d
devJawad1 <adekanbija1@gmail.com>
2024-08-30 16:36:49 +0100

    adding book car page

--- src/pages/BookCarforWash.jsx
+++ src/pages/BookCarforWash.jsx
@@ -116,7 +116,7 @@ const BookCarforWash = () => {
                                         {
                                             !checkSelectedCar(car._id) ?
                                                 <div className={`${oneLocation ? "d-none" : "d-flex"} rounded border border-primary align-items-cnter mt-2`} style={{height:"30px"}}>
-                                                    <input type="text" name="" id="" placeholder='input car location' className={`col-10  p-0 px-2 border-0`} style={{ outline: "none", fontsize:"1px" }} value={carLocationValue[car._id] || ''}
+                                                    <input type="text" name="" id="" placeholder='Car location' className={`col-10  p-0 px-2 border-0 rounded`} style={{ outline: "none", fontsize:"1px" }} value={carLocationValue[car._id] || ''}
                                                     onChange={(e) => handleLocationChange(car._id, e.target.value)} />
 
                                                     <div className='col-2 border bi bi-check bg-primary text-white d-flex justify-content-center align-items-center' onClick={() => { selectCar(car._id, carLocationValue) }}></div>
